f07d2eea98403d14308e61db7570f862f9011de4
zhexuany <zhexuany@gmail.com>
2017-01-23 04:42:38 +0800

    meta: init commit for cluster package

---
+++ cluster/meta_executor.go
@@ -0,0 +1,263 @@
+package cluster
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/influxdata/influxdb"
+	"github.com/influxdata/influxdb/influxql"
+	"github.com/influxdata/influxdb/models"
+	"github.com/influxdata/influxdb/services/meta"
+	"github.com/influxdata/influxdb/tsdb"
+	"github.com/zhexuany/influxdb-cluster/rpc"
+	"github.com/zhexuany/influxdb-cluster/tlv"
+	"io"
+)
+
+const (
+	metaExecutorWriteTimeout        = 5 * time.Second
+	metaExecutorMaxWriteConnections = 10
+)
+
+// MetaExecutor executes meta queries on all data nodes.
+type MetaExecutor struct {
+	mu             sync.RWMutex
+	timeout        time.Duration
+	pool           *clientPool
+	maxConnections int
+	Logger         *log.Logger
+	Node           *influxdb.Node
+
+	nodeExecutor interface {
+		executeOnNode(stmt influxql.Statement, database string, node *meta.NodeInfo) error
+	}
+
+	MetaClient interface {
+		DataNode(id uint64) (ni *meta.NodeInfo, err error)
+		DataNodes() ([]meta.NodeInfo, error)
+	}
+
+	TSDBStore interface {
+		CreateShard(database, retentionPolicy string, shardID uint64, enabled bool) error
+		BackupShard(id uint64, since time.Time, w io.Writer) error
+		RestoreShard(id uint64, r io.Reader) error
+		Measurements(database string, cond influxql.Expr) ([]string, error)
+		TagValues(database string, cond influxql.Expr) ([]tsdb.TagValues, error)
+	}
+
+	ShardWriter interface {
+		WriteShard(shardID, ownerID uint64, points []models.Point) error
+	}
+}
+
+// NewMetaExecutor returns a new initialized *MetaExecutor.
+func NewMetaExecutor() *MetaExecutor {
+	m := &MetaExecutor{
+		timeout:        metaExecutorWriteTimeout,
+		pool:           newClientPool(),
+		maxConnections: metaExecutorMaxWriteConnections,
+		Logger:         log.New(os.Stderr, "[meta-executor] ", log.LstdFlags),
+	}
+	m.nodeExecutor = m
+
+	return m
+}
+
+//
+//
+
+// remoteNodeError wraps an error with context about a node that
+// returned the error.
+type remoteNodeError struct {
+	id  uint64
+	err error
+}
+
+func (e remoteNodeError) Error() string {
+	return fmt.Sprintf("partial success, node %d may be down (%s)", e.id, e.err)
+}
+
+// ExecuteStatement executes a single InfluxQL statement on all nodes in the cluster concurrently.
+func (m *MetaExecutor) ExecuteStatement(stmt influxql.Statement, database string) error {
+	// Get a list of all nodes the query needs to be executed on.
+	nodes, err := m.MetaClient.DataNodes()
+	if err != nil {
+		return err
+	} else if len(nodes) < 1 {
+		return nil
+	}
+
+	// Start a goroutine to execute the statement on each of the remote nodes.
+	var wg sync.WaitGroup
+	errs := make(chan error, len(nodes)-1)
+	for _, node := range nodes {
+		wg.Add(1)
+		go func(node meta.NodeInfo) {
+			defer wg.Done()
+			if err := m.nodeExecutor.executeOnNode(stmt, database, &node); err != nil {
+				errs <- remoteNodeError{id: node.ID, err: err}
+			}
+		}(node)
+	}
+
+	// Wait on n-1 nodes to execute the statement and respond.
+	wg.Wait()
+
+	select {
+	case err = <-errs:
+		return err
+	default:
+		return nil
+	}
+}
+
+// executeOnNode executes a single InfluxQL statement on a single node.
+func (m *MetaExecutor) executeOnNode(stmt influxql.Statement, database string, node *meta.NodeInfo) error {
+	// We're executing on a remote node so establish a connection.
+	c, err := m.dial(node.ID)
+	if err != nil {
+		return err
+	}
+
+	conn, ok := c.(*pooledConn)
+	if !ok {
+		panic("wrong connection type in MetaExecutor")
+	}
+	// Return connection to pool by "closing" it.
+	defer conn.Close()
+
+	// Build RPC request.
+	var request rpc.ExecuteStatementRequest
+	request.SetStatement(stmt.String())
+	request.SetDatabase(database)
+
+	// Marshal into protocol buffer.
+	buf, err := request.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	// Send request.
+	conn.SetWriteDeadline(time.Now().Add(m.timeout))
+	if err := tlv.WriteTLV(conn, tlv.ExecuteStatementRequestMessage, buf); err != nil {
+		conn.MarkUnusable()
+		return err
+	}
+
+	// Read the response.
+	conn.SetReadDeadline(time.Now().Add(m.timeout))
+	// TODO need migrte readTLV from cluster to tlv
+	// it is just temporary solution
+	_, buf, err = ReadTLV(conn)
+	if err != nil {
+		conn.MarkUnusable()
+		return err
+	}
+
+	// Unmarshal response.
+	var response rpc.ExecuteStatementResponse
+	if err := response.UnmarshalBinary(buf); err != nil {
+		return err
+	}
+
+	if response.Code() != 0 {
+		return fmt.Errorf("error code %d: %s", response.Code(), response.Message())
+	}
+
+	return nil
+}
+
+// dial returns a connection to a single node in the cluster.
+func (m *MetaExecutor) dial(nodeID uint64) (net.Conn, error) {
+	// If we don't have a connection pool for that addr yet, create one
+	_, ok := m.pool.getPool(nodeID)
+	if !ok {
+		factory := &connFactory{nodeID: nodeID, clientPool: m.pool, timeout: m.timeout}
+		factory.metaClient = m.MetaClient
+
+		p, err := NewBoundedPool(1, m.maxConnections, m.timeout, factory.dial)
+		if err != nil {
+			return nil, err
+		}
+		m.pool.setPool(nodeID, p)
+	}
+	return m.pool.conn(nodeID)
+}
+
+func (m *MetaExecutor) CreateShard(db, policy string, shardID uint64, enabled bool) error {
+	return m.TSDBStore.CreateShard(db, policy, shardID, enabled)
+}
+
+func (m *MetaExecutor) WriteToShard(shardID, ownerID uint64, points []models.Point) error {
+	return m.ShardWriter.WriteShard(shardID, ownerID, points)
+}
+
+func (m *MetaExecutor) DeleteDatabase(stmt influxql.Statement) error {
+	db := ""
+	return m.ExecuteStatement(stmt, db)
+}
+
+func (m *MetaExecutor) DeleteMeasurement(stmt influxql.Statement) error {
+	db := ""
+	return m.ExecuteStatement(stmt, db)
+}
+
+func (m *MetaExecutor) DeleteRetentionPolicy(stmt influxql.Statement) error {
+	db := ""
+	if st, ok := stmt.(*influxql.DropRetentionPolicyStatement); ok {
+		db = st.Database
+	}
+	return m.ExecuteStatement(stmt, db)
+}
+
+func (m *MetaExecutor) DeleteSeries(stmt influxql.Statement) error {
+	db := ""
+	if st, ok := stmt.(*influxql.DropSeriesStatement); ok {
+
+	}
+	return m.ExecuteStatement(stmt, db)
+}
+
+func (m *MetaExecutor) DeleteShard(stmt influxql.Statement) error {
+	db := ""
+	return m.ExecuteStatement(stmt, db)
+}
+
+func (m *MetaExecutor) BackupShard(id uint64, since time.Time, w io.Writer) error {
+	return m.TSDBStore.BackupShard(id, since, w)
+}
+
+func (m *MetaExecutor) RestoreShard(id uint64, r io.Reader) error {
+	return m.TSDBStore.RestoreShard(id, r)
+}
+
+func (m *MetaExecutor) IteratorCreator(opt influxql.IteratorOptions) influxql.IteratorCreator {
+	//
+	ic := remoteIteratorCreator{}
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+
+	//
+	//
+	return nil
+}
+
+func (m *MetaExecutor) Measurements() []string {
+	return nil
+}
+
+func (m *MetaExecutor) TagValues() {
+
+}
+
+func (m *MetaExecutor) MetaIteratorCreator() {
+}
---
+++ cluster/shard_writer.go
@@ -0,0 +1,196 @@
+package cluster
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/influxdata/influxdb/models"
+	"github.com/influxdata/influxdb/services/meta"
+	"github.com/zhexuany/influxdb-cluster/rpc"
+	"github.com/zhexuany/influxdb-cluster/tlv"
+)
+
+// ShardWriter writes a set of points to a shard.
+type ShardWriter struct {
+	pool           *clientPool
+	timeout        time.Duration
+	maxConnections int
+
+	MetaClient interface {
+		ShardOwner(shardID uint64) (database, policy string, sgi *meta.ShardGroupInfo)
+	}
+}
+
+// NewShardWriter returns a new instance of ShardWriter.
+func NewShardWriter(timeout time.Duration, maxConnections int) *ShardWriter {
+	return &ShardWriter{
+		pool:           newClientPool(),
+		timeout:        timeout,
+		maxConnections: maxConnections,
+	}
+}
+
+// WriteShard writes time series points to a shard
+func (w *ShardWriter) WriteShard(shardID, ownerID uint64, points []models.Point) error {
+	buf := make([]byte, 0)
+	for _, p := range points {
+		b, err := p.MarshalBinary()
+		if err != nil {
+			return fmt.Errorf("failed to marshal point: %v", err)
+		}
+		buf = append(buf, b)
+	}
+	return w.WriteShardBinary(shardID, ownerID, buf)
+
+}
+
+// WriteShardBinary writes binary time series points to a shard
+func (w *ShardWriter) WriteShardBinary(shardID, ownerID uint64, buf []byte) error {
+	c, err := w.dial(ownerID)
+	if err != nil {
+		return err
+	}
+
+	conn, ok := c.(*pooledConn)
+	if !ok {
+		panic("wrong connection type")
+	}
+
+	// Close is not literally closing such connects.
+	// Instead, Close will put connects back to pool
+	// which can imporve the memory usage
+	defer func(conn net.Conn) {
+		conn.Close() // return to pool
+	}(conn)
+
+	// Determine the location of this shard and whether it still exists
+	db, rp, sgi := w.MetaClient.ShardOwner(shardID)
+	if sgi == nil {
+		// If we can't get the shard group for this shard, then we need to drop this request
+		// as it is no longer valid.  This could happen if writes were queued via
+		// hinted handoff and we're processing the queue after a shard group was deleted.
+		return nil
+	}
+
+	// Build write request.
+	var request rpc.WriteShardRequest
+	request.SetShardID(shardID)
+	request.SetDatabase(db)
+	request.SetRetentionPolicy(rp)
+	request.SetBinaryPoints(buf)
+
+	// Marshal into protocol buffers.
+	reqB, err := request.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	// Write request.
+	conn.SetWriteDeadline(time.Now().Add(w.timeout))
+	if err := tlv.WriteTLV(conn, tlv.WriteShardRequestMessage, reqB); err != nil {
+		conn.MarkUnusable()
+		return err
+	}
+
+	// Flush all buffered data
+	if err := bufio.NewWriter(conn).Flush(); err != nil {
+		return err
+	}
+
+	// Read the response.
+	conn.SetReadDeadline(time.Now().Add(w.timeout))
+	_, buf, err = ReadTLV(conn)
+	if err != nil {
+		conn.MarkUnusable()
+		return err
+	}
+
+	// Unmarshal response.
+	var response WriteShardResponse
+	if err := response.UnmarshalBinary(buf); err != nil {
+		return err
+	}
+
+	if response.Code() != 0 {
+		return fmt.Errorf("error code %d: %s", response.Code(), response.Message())
+	}
+
+	return nil
+}
+
+func (w *ShardWriter) dial(nodeID uint64) (net.Conn, error) {
+	// If we don't have a connection pool for that addr yet, create one
+	_, ok := w.pool.getPool(nodeID)
+	if !ok {
+		factory := &connFactory{nodeID: nodeID, clientPool: w.pool, timeout: w.timeout}
+		factory.metaClient = w.MetaClient
+
+		p, err := NewBoundedPool(1, w.maxConnections, w.timeout, factory.dial)
+		if err != nil {
+			return nil, err
+		}
+		w.pool.setPool(nodeID, p)
+	}
+	return w.pool.conn(nodeID)
+}
+
+// Close closes ShardWriter's pool
+func (w *ShardWriter) Close() error {
+	if w.pool == nil {
+		return fmt.Errorf("client already closed")
+	}
+	w.pool.close()
+	w.pool = nil
+	return nil
+}
+
+const (
+	maxConnections = 500
+	maxRetries     = 3
+)
+
+var errMaxConnectionsExceeded = fmt.Errorf("can not exceed max connections of %d", maxConnections)
+
+type connFactory struct {
+	nodeID  uint64
+	timeout time.Duration
+
+	clientPool interface {
+		size() int
+	}
+
+	metaClient interface {
+		DataNode(id uint64) (ni *meta.NodeInfo, err error)
+	}
+}
+
+func (c *connFactory) dial() (net.Conn, error) {
+	if c.clientPool.size() > maxConnections {
+		return nil, errMaxConnectionsExceeded
+	}
+
+	ni, err := c.metaClient.DataNode(c.nodeID)
+	if err != nil {
+		return nil, err
+	}
+
+	if ni == nil {
+		return nil, fmt.Errorf("node %d does not exist", c.nodeID)
+	}
+
+	conn, err := net.DialTimeout("tcp", ni.TCPHost, c.timeout)
+	if err != nil {
+		return nil, err
+	}
+
+	// Write a marker byte for cluster messages.
+	_, err = conn.Write([]byte{MuxHeader})
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return conn, nil
+}
