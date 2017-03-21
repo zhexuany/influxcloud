ca076b096de9863222aa5178eae06dce05a29c8e
zhexuany <zhexuany@gmail.com>
2017-03-21 16:29:05 +0800

    *: added influxdb-cluster

---
+++ influxdb-cluster.go
@@ -0,0 +1,4 @@
+// Package influxdb is the root package of InfluxDB,
+// the scalable datastore for metrics, events, and real-time analytics.
+//
+package influxdb_cluster// import "github.com/zhexuany/influxdb-cluster"
