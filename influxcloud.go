fcc96827c8bfa11a4963f5672287bd7915cab11d
zhexuany <zhexuany@gmail.com>
2017-04-21 11:16:38 +0800

    fixed expects import

--- influxcloud.go
+++ influxcloud.go
@@ -1,4 +1,4 @@
-// Package influxcloud is the root package of InfluxCloud,
+// Package influxcloud is the root package of Influxcloud,
 // the scalable datastore for metrics, events, and real-time analytics.
 //
-package influxcloud // import "github.com/influxcloud/influxcloud"
+package influxcloud // import "github.com/zhexuany/influxcloud"
