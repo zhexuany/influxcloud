ac3176bceec27793b2e8b7e66e2cbe7f6d202895
zhexuany <zhexuany@gmail.com>
2017-04-21 17:12:49 +0800

    cluster: corrected execute statement

--- cluster/statement_executor.go
+++ cluster/statement_executor.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/influxdata/influxdb/coordinator"
 	"github.com/influxdata/influxdb/influxql"
+	"github.com/influxdata/influxdb/models"
 	"github.com/influxdata/influxdb/services/meta"
 	"github.com/zhexuany/influxcloud/rpc"
 	"github.com/zhexuany/influxcloud/tlv"
@@ -22,6 +23,7 @@ type StatementExecutor struct {
 		DataNodes() (ni meta.NodeInfos, err error)
 	}
 
+	// This reprsents local StatementExecutor
 	StatementExecutor coordinator.StatementExecutor
 }
 
@@ -29,14 +31,14 @@ type StatementExecutor struct {
 func (e *StatementExecutor) ExecuteStatement(stmt influxql.Statement, ctx influxql.ExecutionContext) error {
 	switch t := stmt.(type) {
 	case *influxql.ShowQueriesStatement:
-		return e.executeShowQueriesStatement(t)
+		return e.executeShowQueriesStatement(t, ctx)
 	case *influxql.KillQueryStatement:
-		return e.executeKillQueryStatement(t)
+		return e.executeKillQueryStatement(t, ctx)
 	}
 	return e.StatementExecutor.ExecuteStatement(stmt, ctx)
 }
 
-func (e *StatementExecutor) executeShowQueriesStatement(stmt *influxql.ShowQueriesStatement) error {
+func (e *StatementExecutor) executeShowQueriesStatement(stmt *influxql.ShowQueriesStatement, ctx influxql.ExecutionContext) error {
 	dataNodes, err := e.MetaClient.DataNodes()
 	if err != nil {
 		return err
@@ -44,12 +46,12 @@ func (e *StatementExecutor) executeShowQueriesStatement(stmt *influxql.ShowQueri
 
 	for _, data := range dataNodes {
 		go func(data meta.NodeInfo) error {
-			conn, err := net.Dial("tcp", data.TCPHost)
+			conn, err := e.pool.conn(data.ID)
 			if err != nil {
 				return fmt.Errorf("failed to connect:%v", err)
 			}
 			req := rpc.ExecuteStatementRequest{}
-			req.SetDatabase("")
+			req.SetDatabase(ctx.Database)
 			req.SetStatement(stmt.String())
 			err = tlv.EncodeTLV(conn, tlv.ExecuteStatementRequestMessage, &req)
 			if err != nil {
@@ -68,6 +70,33 @@ func (e *StatementExecutor) executeShowQueriesStatement(stmt *influxql.ShowQueri
 	return nil
 }
 
-func (e *StatementExecutor) executeKillQueryStatement(q *influxql.KillQueryStatement) error {
-	return nil
+func (e *StatementExecutor) executeKillQueryStatement(stmt *influxql.KillQueryStatement, ctx influxql.ExecutionContext) error {
+	dataNodes, err := e.MetaClient.DataNodes()
+	if err != nil {
+		return err
+	}
+
+	for _, data := range dataNodes {
+		go func(data meta.NodeInfo) error {
+			conn, err := e.pool.conn(data.ID)
+			if err != nil {
+				return fmt.Errorf("failed to connect:%v", err)
+			}
+			req := rpc.ExecuteStatementRequest{}
+			req.SetDatabase(ctx.Database)
+			req.SetStatement(stmt.String())
+			err = tlv.EncodeTLV(conn, tlv.ExecuteStatementRequestMessage, &req)
+			if err != nil {
+				return fmt.Errorf("failed to encode tlv: %v", err)
+			}
+
+			res := rpc.ExecuteStatementResponse{}
+			if _, err := tlv.DecodeTLV(conn, &res); err != nil {
+				return fmt.Errorf("failed to decode tlv: %v", err)
+			} else if res.Code() != 200 {
+				return fmt.Errorf("response status is not correct")
+			}
+			return nil
+		}(data)
+	}
 }
